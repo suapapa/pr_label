418ae138ebf4ce36089198f70350f393f99d7704
Homin Lee <ff4500@gmail.com>
2022-09-19 16:46:02 +0900

    apply orderQ to API handler

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
 )
 
+var (
+	q *OrderQ
+)
+
 func main() {
+	q = NewOrderQ(DefaultOrderQLen)
+	go printLoop()
+
 	r := gin.Default()
 	v1 := r.Group("v1")
 	{
@@ -24,13 +33,33 @@ func v1OrderHandler(c *gin.Context) {
 			"msg":    errors.Wrap(err, "print order failed"),
 		}
 		c.JSON(http.StatusBadRequest, ret)
+		return
 	}
 
-	printAddrFrom(ord.From)
-	printAddrTo(ord.To)
+	if err := q.Push(&ord); err != nil {
+		ret := map[string]any{
+			"status": http.StatusInternalServerError,
+			"msg":    errors.Wrap(err, "print order failed"),
+		}
+		c.JSON(http.StatusInternalServerError, ret)
+		return
+	}
 
 	ret := map[string]any{
 		"status": http.StatusOK,
 	}
 	c.JSON(http.StatusOK, ret)
+}
+
+func printLoop() {
+	for !q.IsEmpty() {
+		ord, err := q.Pop()
+		if err != nil {
+			log.Printf("ERR: %v", errors.Wrap(err, "print will be stopped"))
+			return
+		}
+		printAddrFrom(ord.From)
+		printAddrTo(ord.To)
+		time.Sleep(time.Second)
+	}
 }
